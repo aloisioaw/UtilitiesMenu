25efbf5999572b4dc9e47cfa52badb664420767b
Aloisio Arsego Wohlmeister <aloisioaw@gmail.com>
2016-05-17 22:56:21 -0300

    Melhorias

--- UtilityMenu.py
+++ UtilityMenu.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # This code is an example for a tutorial on Ubuntu Unity/Gnome AppIndicators:
 # http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html
 
@@ -32,6 +35,10 @@ def build_menu():
 	item_named_restart.connect('activate', service_named_restart)
 	menu.append(item_named_restart)
 
+	item_agnclientd_restart = gtk.MenuItem('Agnclientd Restart')
+	item_agnclientd_restart.connect('activate', service_agnclientd_restart)
+	menu.append(item_agnclientd_restart)
+
 	item_quit = gtk.MenuItem('Fechar')
 	item_quit.connect('activate', quit)
 	menu.append(item_quit)
@@ -39,12 +46,26 @@ def build_menu():
 	return menu
 
 def service_named_restart(_):
-	root=Tk()
-	m=window.popupWindow(root)
-	root.mainloop()
-	#tkMessageBox.showinfo(title="Greetings", message="Hello World!")
-	subprocess.call("echo " + m.senha + " > sudo -S service named restart", shell=True)
-	#notify.Notification.new("<b>Joke</b>", fetch_joke(), None).show()
+	resultado = None
+	while resultado == None or resultado == 1:
+		root=Tk()
+		m=window.popupWindow(root)
+		root.mainloop()
+		if m.senha != None:
+			resultado=subprocess.call("echo " + m.senha + " | sudo -S service named restart", shell=True)
+			if resultado != 1:
+				notify.Notification.new("Reinicialização de Serviço", "Serviço named reiniciado", None).show()
+
+def service_agnclientd_restart(_):
+	resultado = None
+	while resultado == None or resultado == 1:
+		root=Tk()
+		m=window.popupWindow(root)
+		root.mainloop()
+		if m.senha != None:
+			resultado=subprocess.call("echo " + m.senha + " | sudo -S service agnclientd restart", shell=True)
+			if resultado != 1:
+				notify.Notification.new("Reinicialização de Serviço", "Serviço agnclientd reiniciado", None).show()
 
 def quit(_):
 	notify.uninit()
--- window.py
+++ window.py
@@ -9,17 +9,22 @@ class popupWindow(object):
 		self.master.title("Senha de Root")
 		self.lblSenha = Label(master,text = "Senha:")
 		self.lblSenha.grid(row = 0, column = 0, columnspan=2)
-		self.txtSenha = Entry(master, show="*")
+		self.txtSenha = Entry(master, width=25, show="*")
 		self.txtSenha.bind("<Return>", self.pressionou_enter)
-		self.txtSenha.grid(row = 1, column = 0, columnspan=2)
-		self.btnOk = Button(master,text = 'Ok',command = self.cleanup).grid(row = 2, column = 0)
-		self.btnCancelar = Button(master,text = 'Cancelar',command = self.cancelar).grid(row = 2, column = 1)
+		self.txtSenha.bind("<Escape>", self.pressionou_esc)
+		self.txtSenha.grid(row = 1, column = 0, columnspan=2, padx=5, pady=10)
+		self.txtSenha.focus()
+		self.btnOk = Button(master,text = 'Ok', width=8, command = self.cleanup).grid(row = 2, column = 0, padx=5, pady=5)
+		self.btnCancelar = Button(master,text = 'Cancelar', width=8, command = self.cancelar).grid(row = 2, column = 1, padx=5, pady=5)
 
 	def cleanup(self):
 		self.senha = self.txtSenha.get()
 		self.master.destroy()
 
 	def cancelar(self):
+		self.master.destroy()
+
+	def pressionou_esc(self, event):
 		self.master.destroy()
 
 	def pressionou_enter(self, event):
